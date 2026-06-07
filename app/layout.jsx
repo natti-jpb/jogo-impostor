be806f2d8c50d2131dc1b827c40d66ab8391a439
natti-jpb <jpbbonatti@gmail.com>
2026-06-06 22:59:27 -0300

    style: redesign with 8-bit pixel art aesthetic (Press Start 2P, neon palette, chunky borders)

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,6 +8,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&family=VT323&display=swap" rel="stylesheet" />
+      </head>
       <body>{children}</body>
     </html>
   );
